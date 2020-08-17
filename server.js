b397196fb4cd6f051e570c70bec6ed3c6ad6dd73
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-17 13:39:59 -0500

    port added

--- server.js
+++ server.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const notes = require('./db/db.json');
-
+const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.get('/api/db', (req, res) => {
     res.json(notes);
   });
-app.listen(3001, () => {
-    console.log(`API server now on port 3001!`);
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
 });
