263b85c8783e85468cc13730d14512cc930eb74b
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-17 14:59:21 -0500

    i fucked up

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 const express = require('express');
-const notes = require('./db/db.json');
 const PORT = process.env.PORT || 3001;
 const app = express();
+const notes = require('./db/db.json');
 
-app.get('/api/db', (req, res) => {
+
+app.get('/api/notes', (req, res) => {
     res.json(notes);
-  });
+});
+
+app.get('/api/notes', (req, res) => {
+    let results = notes;
+    console.log(req.query)
+    res.json(results);
+});
 app.listen(PORT, () => {
-    console.log(`API server now on port ${PORT}!`);
+    console.log(`API server now on ${PORT}`);
 });
