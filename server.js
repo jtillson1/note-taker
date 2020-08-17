af8906cdac6593b1499b9231baab0fac7dfbd47f
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-17 16:31:18 -0500

    neverfuckingmind

--- server.js
+++ server.js
@@ -5,17 +5,17 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 const { notes } = require('./db/db.json');
 
-app.use(express.urlencoded({ extended: true}));
+app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static('public'));
 
 function createNewNote(body, notesArray) {
     const note = body;
     notesArray.push(note);
-fs.writeFileSync(
-    path.join(__dirname, './db/db.json'),
-    JSON.stringify({ notes: notesArray }, null, 2)
-);
+    fs.writeFileSync(
+        path.join(__dirname, './db/db.json'),
+        JSON.stringify({ notes: notesArray }, null, 2)
+    );
     return note;
 };
 
@@ -26,16 +26,16 @@ app.get('/api/notes', (req, res) => {
 app.post('/api/notes', (req, res) => {
     //add note to json file and notes array
     const note = createNewNote(req.body, notes);
-    
+
     res.json(note);
 });
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, './public/index.html'));
-  });
-  app.get('/notes', (req, res) => {
+});
+app.get('/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './public/notes.html'));
-  });
+});
 
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}`);
