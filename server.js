a7e076f87932933ca4e7b25a7545b4e741516344
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-17 13:38:24 -0500

    Add heroku

---
+++ server.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const notes = require('./db/db.json');
+
+const app = express();
+
+app.get('/api/db', (req, res) => {
+    res.json(notes);
+  });
+app.listen(3001, () => {
+    console.log(`API server now on port 3001!`);
+});
