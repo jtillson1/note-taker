96347b8016886c248d7f14fd4b1c29bdc712ec9f
jtillson1 <65985044+jtillson1@users.noreply.github.com>
2020-08-17 15:57:57 -0500

    made 'public' files readily available

--- server.js
+++ server.js
@@ -1,18 +1,42 @@
+const fs = require('fs');
+const path = require('path');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
-const notes = require('./db/db.json');
+const { notes } = require('./db/db.json');
 
+app.use(express.urlencoded({ extended: true}));
+app.use(express.json());
+app.use(express.static('public'));
+
+function createNewNote(body, notesArray) {
+    const note = body;
+    notesArray.push(note);
+fs.writeFileSync(
+    path.join(__dirname, './db/db.json'),
+    JSON.stringify({ notes: notesArray }, null, 2)
+);
+    return note;
+}
 
 app.get('/api/notes', (req, res) => {
     res.json(notes);
 });
 
-app.get('/api/notes', (req, res) => {
-    let results = notes;
-    console.log(req.query)
-    res.json(results);
+app.post('/api/notes', (req, res) => {
+    //add note to json file and notes array
+    const note = createNewNote(req.body, notes);
+    
+    res.json(note);
 });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+  });
+  app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/notes.html'));
+  });
+
 app.listen(PORT, () => {
     console.log(`API server now on ${PORT}`);
 });
